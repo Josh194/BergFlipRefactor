fb5c315b8f16c24d32dd2df77baca9b5a0c6681f
Bergen Cloninger <bergencloninger04@gmail.com>
2024-04-17 12:50:40 -0600

    Started work on model (currently not working), creates database and tables but insert logic gives an exception.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test code");
+        Model model = new Model();
+        model.createInitialUserTable();
+        model.createInitialLeaderboard();
+
+        model.addUser("bergen", "123456");
     }
 }
